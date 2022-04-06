25171845b18f65973b2a1394e1745ba7a135f672
Shubham <forevershubham123@gmail.com>
2022-04-06 21:22:15 +0530

    Updates

---
+++ precache-manifest.388aa311e4ed393ebb465ef31564e179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b69892833c5becffa8986dbf313ced",
+    "url": "/auspicious123.github.io/login-signup-react/index.html"
+  },
+  {
+    "revision": "74d0705b7aae9efacdf0",
+    "url": "/auspicious123.github.io/login-signup-react/static/css/main.8ea132df.chunk.css"
+  },
+  {
+    "revision": "6d1c158b9d917274ef7c",
+    "url": "/auspicious123.github.io/login-signup-react/static/js/2.3b0248f5.chunk.js"
+  },
+  {
+    "revision": "74d0705b7aae9efacdf0",
+    "url": "/auspicious123.github.io/login-signup-react/static/js/main.ca53b708.chunk.js"
+  },
+  {
+    "revision": "59e5d044176a1dcc0810",
+    "url": "/auspicious123.github.io/login-signup-react/static/js/runtime~main.65a80749.js"
+  },
+  {
+    "revision": "37207bcc01856a6f46a03896a46de5db",
+    "url": "/auspicious123.github.io/login-signup-react/static/media/bg.37207bcc.png"
+  }
+]);
